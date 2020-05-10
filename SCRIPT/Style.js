dd9fd57a153026ad89d2d84d3e8b48568b4dbf3c
BrGUILHERMEHenrique <guilhermehenriquersouza17@gmail.com>
2020-05-10 17:04:03 -0300

    Botão Portifólio estilizado e usado para nova funcionalidade, direcinando para outra página do projeto

--- SCRIPT/Style.js
+++ SCRIPT/Style.js
@@ -14,35 +14,31 @@ menu.style.display = 'none';
 menu.style.border = 'none';
 menu.style.borderRadius = '5px';
 
-var proj = document.querySelector('body div#menu div#P button')
+var proj = document.querySelector('body div#menu div#Btnsmenu button#pág')
 proj.style.textDecoration = 'none';
 proj.style.color = 'black';
 proj.style.transform = 'uppercase';
 proj.style.fontFamily = "Verdana";
-proj.style.fontSize = '12px';
+proj.style.fontSize = '20px';
 proj.style.backgroundColor = 'transparent';
 proj.style.border = 'none';
-proj.style.width = '10%';
-proj.style.height = '20%';
-proj.style.marginLeft = '20%';
+proj.style.marginLeft = '0';
 
 
-var port = document.querySelector('body div#menu button');
+var port = document.querySelector('body div#Btnsmenu button#port');
 
-port .style.textDecoration = 'none';
+port.style.textDecoration = 'none';
 port.style.color = 'black';
-port.style.width = '10%';
-port.style.height = '10%';
+port.style.fontSize = '20px';
 port.style.background = 'transparent';
 port.style.border = 'none';
-port.style.marginLeft = '7%';
+port.style.marginTop = '25px';
+port.style.marginLeft = '20px';
 
 var btns = document.querySelector('body div#btns');
 
 btns.style.width = '20px';
 btns.style.height = '15px';
-
-console.log(btns);
 
 
 
@@ -77,27 +73,25 @@ MenuButton3.style.marginBottom = '2px';
 
 function menuaberto() {
 	menu.style.display = 'block';
-	MenuButton2.style.transform = 'rotate(45deg)';
-	MenuButton2.style.backgroundColor = 'blue';
+	MenuButton2.style.display = 'none';
 	MenuButton3.style.display = 'none';
-	MenuButton.style.transform = 'rotate(-45deg)';
-	MenuButton.style.backgroundColor = 'blue';
+	MenuButton.style.display = 'none';
 	
+}
+
+
+
+function menufechado() {
+	menu.style.display = 'none';
+	MenuButton2.style.display = 'block';
+	MenuButton3.style.display = 'block';
+	MenuButton.style.display = 'block';
+
+
 }
 
 btns.onmouseover = function menu(){
 	menuaberto();
-}
-
-function menufechado() {
-	menu.style.display = 'none';
-	MenuButton2.style.transform = 'none';
-	MenuButton2.style.backgroundColor = 'black';
-	MenuButton3.style.display = 'block';
-	MenuButton.style.transform = 'none';
-	MenuButton.style.backgroundColor = 'black';
-
-
 }
 
 
@@ -125,13 +119,28 @@ proj.onclick = function ( ) {
 
 proj.onmouseover = function (){
 	proj.style.height = '20%';
-	proj.style.fontSize = '24px';
+	proj.style.fontSize = '30px';
 	proj.style.color = 'white';
+	proj.style.backgroundColor = 'rgba(0, 0, 0, 10%)';
+	menu.style.msFilter = 'blur(20px)';
 
 }
 
 proj.onmouseout = function () {
-	proj.style.fontSize = '12px';
+	proj.style.fontSize = '20px';
 	proj.style.color = 'black';
+	proj.style.backgroundColor = 'transparent';
 }
 
+port.onmouseover =  function() {
+	port.style.fontSize = '24px';
+	port.style.color = 'white';
+}
+
+port.onmouseout = function ( ) {
+	port.style.fontSize = '20px' ;
+	port.style.color = 'black';
+}
+port.onclick = function ( ){
+	window.open('projetos.html');
+}
